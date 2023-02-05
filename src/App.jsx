54ccc76ed590df7dcfa650ee5ded88a139706484
Muhammad Yizimu <64603940+MuhammadYildiz@users.noreply.github.com>
2023-02-05 19:25:20 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
 	const [active, setActive] = useState("")
 	return (
 		<div className='w-[90%] m-auto bg-cyan-500 p-5 sm:w-[50%] mt-20 rounded-xl shadow-2xl ' >
-			<h1 className='text-center text-yellow-50 text-3xl p- '>FAQ APP</h1>
+			<h1 className='text-center text-yellow-50 text-3xl py-5'>FAQ APP</h1>
 			<FAQ title="Question  1" active={active} setActive={setActive}
 				text="1 Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, libero Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugiat, ipsam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, molestias?" />
 			<FAQ title="Question  2" active={active} setActive={setActive}
