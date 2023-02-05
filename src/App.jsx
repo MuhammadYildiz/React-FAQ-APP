13b10bbc56095f6740f809debf6d288218917cee
Muhammad Yizimu <64603940+MuhammadYildiz@users.noreply.github.com>
2023-02-05 19:20:50 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,16 @@ function App() {
 	return (
 		<div className='w-[90%] m-auto bg-cyan-500 p-5 sm:w-[50%] mt-20 rounded-xl shadow-2xl ' >
 			<h1 className='text-center text-yellow-50 text-3xl p- '>FAQ APP</h1>
-			<FAQ title="Question  1" active ={active} setActive ={setActive} />
-			<FAQ title="Question  2" active ={active} setActive ={setActive} />
-			<FAQ title="Question  3" active ={active} setActive ={setActive} />
-			<FAQ title="Question  4" active ={active} setActive ={setActive} />
-			<FAQ title="Question  5" active ={active} setActive ={setActive} />
+			<FAQ title="Question  1" active={active} setActive={setActive}
+				text="1 Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, libero Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugiat, ipsam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, molestias?" />
+			<FAQ title="Question  2" active={active} setActive={setActive}
+				text="2 Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, libero Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugiat, ipsam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, molestias?" />
+			<FAQ title="Question  3" active={active} setActive={setActive}
+				text="3 Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, libero Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugiat, ipsam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, molestias?" />
+			<FAQ title="Question  4" active={active} setActive={setActive}
+				text="4 Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, libero Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugiat, ipsam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, molestias?" />
+			<FAQ title="Question  5" active={active} setActive={setActive}
+				text="5 Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, libero Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugiat, ipsam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, molestias?" />
 		</div>
 	)
 }
